50e101fccb221b266490ec3b778883cb4afffdd7
devxprite <devxprite@gmail.com>
2023-05-25 20:56:51 +0530

    fix font

--- app/layout.js
+++ app/layout.js
@@ -42,6 +42,8 @@ export default async function RootLayout({ children }) {
        <head>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;800;900&family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet" />
        </head>
         <body>
           <main>
