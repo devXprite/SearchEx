9f0346603ac5b1203332beb8e7c5a3806d19a5f1
devxprite <devxprite@gmail.com>
2023-06-03 14:40:08 +0530

    update

--- app/account/search-history/page.js
+++ app/account/search-history/page.js
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth"
 import mongoClient from "@/utils/db"
 import Link from "next/link";
 import styles from "./page.module.scss";
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
 
 const formarTimeStamp = (timestamp) => {
     const date = new Date(timestamp);
@@ -10,16 +11,21 @@ const formarTimeStamp = (timestamp) => {
 }
 
 export default async function Page() {
-    const session = await getServerSession();
+
+    const session = await getServerSession(authOptions);
     const email = session?.user?.email;
     const name = session?.user?.name || session?.user?.email.split('@')[0];
 
     const client = await mongoClient;
     const db = client.db();
 
+    console.log(email);
+
     const searchHistory = await db.collection("history").find({
         email: email
     }).limit(200).toArray();
+
+
 
     console.log(searchHistory);
 
@@ -28,21 +34,41 @@ export default async function Page() {
             <h2> Search History of {name} </h2>
 
             {(searchHistory.length > 0)
-                ? (<div className={styles.history}>
-                    {searchHistory.map((item, index) => (
-                        <p className={styles.item} key={index}>
-                            <span className={styles.item__timestamp}>
-                                {(item.localTimestamp)}
-                            </span>
-                            <span className={styles.item__query}>
-                                {item.query}
-                            </span>
-                            <span className={styles.item__path}>
-                                {item.path}
-                            </span>
-                        </p>
-                    ))}
-                </div>)
+                ? (
+                    <div className={styles.history}>
+                        {searchHistory.map((item, index) => (
+                            <p className={styles.item} key={index}>
+                                <span className={styles.item__timestamp}>
+                                    {(item.localTimestamp)}
+                                </span>
+                                <span className={styles.item__query}>
+                                    {item.query}
+                                </span>
+                                <span className={styles.item__path}>
+                                    {item.path}
+                                </span>
+                            </p>
+                        ))}
+                    </div>
+                    // <table>
+                    //     <thead>
+                    //         <tr>
+                    //             <th>Timestamp</th>
+                    //             <th>Query</th>
+                    //             <th>Path</th>
+                    //         </tr>
+                    //     </thead>
+                    //     <tbody>
+                    //         {searchHistory.map((item, index) => (
+                    //             <tr key={index}>
+                    //                 <td>{formarTimeStamp(item.localTimestamp).toLocaleString()}</td>
+                    //                 <td>{item.query}</td>
+                    //                 <td>{item.path}</td>
+                    //             </tr>
+                    //         ))}
+                    //     </tbody>
+                    // </table>
+                )
                 : (<p>No Search History found!</p>)
             }
 
